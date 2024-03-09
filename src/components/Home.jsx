27398118b47f6e187107372a39aa7dc151be2820
Gustavo <gmerida@correo.unimet.edu.ve>
2024-03-08 22:01:32 -0400

    Componentes

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Esta es la página principal</h1>
+    </div>
+  )
+}
+
+export default Home
