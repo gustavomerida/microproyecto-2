f0804f10706f8000f95822449f1db8b601cdd19a
Daniel Fiallo <daniel.fiallo@correo.unimet.edu.ve>
2024-03-09 12:40:12 -0400

    Base de datos cargada

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 /* eslint-disable no-unused-vars */
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.jsx'
-import './index.css'
-import {router} from './router.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { router } from "./router.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import { useState } from "react";
+import { loadGameData, loadClubData } from "../services/importData";
 
-function Home() {
+/* eslint-disable no-unused-vars */
+export default function Home() {
+  const [id, setID] = useState("");
+  const [title, setTitle] = useState("");
+  const [gender, setGender] = useState("");
+  const [description, setDescription] = useState("");
+
+  async function handleSubmit() {
+    await loadGameData();
+    await loadClubData();
+    alert("Pelicula creada");
+  }
+
   return (
     <div>
-      <h1>Esta es la página principal</h1>
+      <h1>Esta es la página principal jeje</h1>
+      <input value={id} onChange={(e) => setID(e.target.value)} />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input value={gender} onChange={(e) => setGender(e.target.value)} />
+      <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Enviar</button>
     </div>
-  )
+  );
 }
-
-export default Home
