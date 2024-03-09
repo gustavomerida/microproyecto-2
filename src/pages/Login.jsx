2dfe164bce27983871488a0b725413a1b7bec369
Daniel Fiallo <daniel.fiallo@correo.unimet.edu.ve>
2024-03-09 17:12:25 -0400

    Autenticación

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,64 +1,104 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import styles from './Login.module.css'
-import { loginWithCredentials, } from '../controllers/auth';
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { auth, googleProvider } from '../firebase';
-import {signInWithPopup, signInWithRedirect} from 'firebase/auth'
+import React from "react";
+import styles from "./Login.module.css";
+import { loginWithCredentials } from "../controllers/auth";
+import { useEffect, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase.js";
+import { useNavigate } from "react-router-dom";
+import { auth, googleProvider } from "../firebase";
+import {
+  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
+} from "firebase/auth";
 //import Input from '../components/Input'
 //import Button from '../components/Button'
-import { useUser } from '../context/user'
+import { useUser } from "../context/user";
 
 function Login() {
-  
-  const user= useUser();
+  const user = useUser();
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  useEffect(()=>{
-      if(user){
-          navigate('/', {replace : true})
-          console.log(user)
-      }
+  useEffect(() => {
+    if (user) {
+      navigate("/", { replace: true });
+      console.log(user);
+    }
   }, [user, navigate]);
 
-  const handleLogin = async ()=> {
-      const user = await loginWithCredentials(email, password);
-      if (user!== null){
-          navigate('/');
-          console.log(user)
-      }else{
-          alert("No se pudo iniciar sesión");
-      }
+  const handleLogin = async () => {
+    const user = await loginWithCredentials(email, password);
+    if (user !== null) {
+      navigate("/");
+      console.log(user);
+    } else {
+      alert("No se pudo iniciar sesión");
+    }
   };
 
-  const handleLogin_Google = async ()=> {
-    const user = await signInWithRedirect(auth, googleProvider);
-    if (user!== null){
-        navigate('/');
-        console.log(user)
-    }else{
-        alert("No se pudo iniciar sesión");
+  const handleLogin_Google = async () => {
+    const user = await signInWithPopup(auth, googleProvider);
+    if (user !== null) {
+      navigate("/");
+      console.log(user);
+    } else {
+      console.log("No se pudo autenticar al usuario con Google.");
+      alert(
+        "No se pudo iniciar sesión con Google. Por favor, inténtalo de nuevo."
+      );
     }
-};
+  };
 
-return (
-  <div className={styles.loginContainer}> {/* Aplica la clase de estilo al contenedor principal */}
-  <h1 className={styles.loginTitle}>Inicio de sesión</h1> {/* Aplica la clase de estilo al título */}
-  <form>
-      <label>Email:</label>
-      <input className={styles.formInput} type="email" placeholder="Correo electrónico" onChange={(e)=>setEmail(e.target.value)}/> {/* Aplica la clase de estilo al input */}
-      <label>Contraseña:</label>
-      <input className={styles.formInput} type="password" placeholder="Contraseña" onChange={(e)=>setPassword(e.target.value)}/> {/* Aplica la clase de estilo al input */}
-      <button type="button" className={styles.loginButton} onClick={handleLogin} >Iniciar Sesión con Email</button> {/* Aplica la clase de estilo al botón */}
-  </form>
-  <button type="button" className={styles.socialButton} onClick={handleLogin_Google}>Iniciar Sesión con Google</button> {/* Aplica la clase de estilo al botón */}
-  <button type="button" className={styles.socialButton} >Iniciar Sesión con Facebook</button> {/* Aplica la clase de estilo al botón */}
-  </div>
-)
+  return (
+    <div className={styles.loginContainer}>
+      {" "}
+      {/* Aplica la clase de estilo al contenedor principal */}
+      <h1 className={styles.loginTitle}>Inicio de sesión</h1>{" "}
+      {/* Aplica la clase de estilo al título */}
+      <form>
+        <label>Email:</label>
+        <input
+          className={styles.formInput}
+          type="email"
+          placeholder="Correo electrónico"
+          onChange={(e) => setEmail(e.target.value)}
+        />{" "}
+        {/* Aplica la clase de estilo al input */}
+        <label>Contraseña:</label>
+        <input
+          className={styles.formInput}
+          type="password"
+          placeholder="Contraseña"
+          onChange={(e) => setPassword(e.target.value)}
+        />{" "}
+        {/* Aplica la clase de estilo al input */}
+        <button
+          type="button"
+          className={styles.loginButton}
+          onClick={handleLogin}
+        >
+          Iniciar Sesión con Email
+        </button>{" "}
+        {/* Aplica la clase de estilo al botón */}
+      </form>
+      <button
+        type="button"
+        className={styles.socialButton}
+        onClick={handleLogin_Google}
+      >
+        Iniciar Sesión con Google
+      </button>{" "}
+      {/* Aplica la clase de estilo al botón */}
+      <button type="button" className={styles.socialButton}>
+        Iniciar Sesión con Facebook
+      </button>{" "}
+      {/* Aplica la clase de estilo al botón */}
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,73 +1,182 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 //import Input from '../components/Input'
 //import Button from '../components/Button'
-import { useUser } from '../context/user'
-import { registerWithCredentials } from '../controllers/auth'
-import styles from './Register.module.css'
-import { auth, googleProvider } from '../firebase';
-import {signInWithPopup, signInWithRedirect} from 'firebase/auth'
+import { useUser } from "../context/user";
+import { registerWithCredentials } from "../controllers/auth";
+import styles from "./Register.module.css";
+import { auth, googleProvider } from "../firebase";
+import { db } from "../firebase.js";
+import { collection, addDoc } from "firebase/firestore";
+import {
+  getAdditionalUserInfo,
+  signInWithPopup,
+  signInWithRedirect,
+  createUserWithEmailAndPassword,
+  fetchSignInMethodsForEmail,
+} from "firebase/auth";
 
 function Register() {
+  const user = useUser();
+  const [isFieldsEmpty, setIsFieldsEmpty] = useState();
+  const navigate = useNavigate();
 
-    const user= useUser();
-    const navigate = useNavigate();
+  const [nameInput, setNameInput] = useState("");
+  const [lastnameInput, setLastnameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
 
-    const [name, setName] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  useEffect(() => {
+    if (user) {
+      navigate("/", { replace: true });
+      console.log(user);
+    }
+  }, [user, navigate]);
 
-    useEffect(()=>{
-        if(user){
-            navigate('/', {replace : true})
-            console.log(user)
+  const handleLogin = async () => {
+    if (!nameInput || !lastnameInput || !emailInput || !passwordInput) {
+      alert("Por favor, completa todos los campos.");
+      return;
+    }
+    fetchSignInMethodsForEmail(auth, emailInput)
+      .then((signInMethods) => {
+        if (signInMethods.length > 0) {
+          // El correo ya está en uso
+          alert(
+            "Este correo electrónico ya está registrado. Por favor, utiliza otro."
+          );
+        } else {
+          // El correo no está registrado, proceder con el registro
+          return createUserWithEmailAndPassword(
+            auth,
+            emailInput,
+            passwordInput
+          );
         }
-    }, [user, navigate]);
+      })
+      .then((userCredential) => {
+        const user = userCredential.user;
 
-    const handleLogin = async ()=> {
-        const user = await registerWithCredentials(email, password);
+        const userCollection = collection(db, "users");
+        alert("Registro exitoso");
+        return addDoc(userCollection, {
+          name: nameInput,
+          last_name: lastnameInput,
+          username: "",
+          email: emailInput,
+          videogame: "",
+          memberships: "",
+        });
+      })
+      .then(() => {
+        navigate("/");
+        console.log("Registro exitoso");
+      })
+      .catch((error) => {
+        console.error("Error al registrar:", error.message);
+        alert(
+          "No se pudo crear la cuenta porque este correo electrónico ya está registrado. Por favor, inténtalo de nuevo o inicie sesión."
+        );
+      });
+  };
 
-        //Aplicar lógica para guardar datos de usuario...
+  const handleLogin_Google = async () => {
+    const user = await signInWithPopup(auth, googleProvider);
+    const data = getAdditionalUserInfo(user);
 
-        if (user!== null){
-            navigate('/');
-            console.log(user)
-        }else{
-            alert("No se pudo crear la cuenta");
-        }
-    };
+    if (data.isNewUser) {
+      const userCollection = collection(db, "users");
+      const displayName = user.user.displayName;
+      const picture = user.user.photoURL;
+      const [firstName, lastName] = displayName.split(" ");
+      await addDoc(userCollection, {
+        name: firstName,
+        last_name: lastName,
+        username: "",
+        email: user.user.email,
+        videogame: "",
+        memberships: "",
+      });
 
-    const handleLogin_Google = async ()=> {
-        const user = await signInWithRedirect(auth, googleProvider);
-        if (user!== null){
-            navigate('/');
-            console.log(user)
-        }else{
-            alert("No se pudo iniciar sesión");
-        }
-    };
+      console.log("Nombre:", firstName);
+      console.log("Apellido:", lastName);
+      alert("Registro exitoso");
+      navigate("/");
+      console.log(user);
+    } else if (!data.isNewUser) {
+      navigate("/");
+      console.log(user);
+    } else {
+      console.log("No se pudo autenticar al usuario con Google.");
+      alert(
+        "No se pudo iniciar sesión con Google. Por favor, inténtalo de nuevo."
+      );
+    }
+  };
 
   return (
-    <div className={styles.loginContainer}> {/* Aplica la clase de estilo al contenedor principal */}
-    <h1 className={styles.loginTitle}>Registro de usuarios</h1> {/* Aplica la clase de estilo al título */}
-    <form>
+    <div className={styles.loginContainer}>
+      {" "}
+      {/* Aplica la clase de estilo al contenedor principal */}
+      <h1 className={styles.loginTitle}>Registro de usuarios</h1>{" "}
+      {/* Aplica la clase de estilo al título */}
+      <form>
         <label>Nombre:</label>
-        <input className={styles.formInput} type="name" placeholder="Escribe tu nombre" onChange={(e)=>setName(e.target.value)}/>
+        <input
+          className={styles.formInput}
+          type="name"
+          placeholder="Escribe tu nombre"
+          onChange={(e) => setNameInput(e.target.value)}
+        />
         <label>Apellido:</label>
-        <input className={styles.formInput} type="lastname" placeholder="Escribe tu apellido" onChange={(e)=>setLastname(e.target.value)}/>
+        <input
+          className={styles.formInput}
+          type="lastname"
+          placeholder="Escribe tu apellido"
+          onChange={(e) => setLastnameInput(e.target.value)}
+        />
         <label>Email:</label>
-        <input className={styles.formInput} type="email" placeholder="Correo electrónico" onChange={(e)=>setEmail(e.target.value)}/> {/* Aplica la clase de estilo al input */}
+        <input
+          className={styles.formInput}
+          type="email"
+          placeholder="Correo electrónico"
+          onChange={(e) => setEmailInput(e.target.value)}
+        />{" "}
+        {/* Aplica la clase de estilo al input */}
         <label>Contraseña:</label>
-        <input className={styles.formInput} type="password" placeholder="Contraseña" onChange={(e)=>setPassword(e.target.value)}/> {/* Aplica la clase de estilo al input */}
-        <button type="button" className={styles.loginButton} onClick={handleLogin} >Iniciar Sesión con Email</button> {/* Aplica la clase de estilo al botón */}
-    </form>
-    <button type="button" className={styles.socialButton} onClick={handleLogin_Google}>Iniciar Sesión con Google</button> {/* Aplica la clase de estilo al botón */}
-    <button type="button" className={styles.socialButton} >Iniciar Sesión con Facebook</button> {/* Aplica la clase de estilo al botón */}
+        <input
+          className={styles.formInput}
+          type="password"
+          placeholder="Contraseña"
+          onChange={(e) => setPasswordInput(e.target.value)}
+        />{" "}
+        {/* Aplica la clase de estilo al input */}
+        <button
+          type="button"
+          className={styles.loginButton}
+          onClick={handleLogin}
+          disabled={isFieldsEmpty}
+        >
+          Registrarse con Email
+        </button>{" "}
+        {/* Aplica la clase de estilo al botón */}
+      </form>
+      <button
+        type="button"
+        className={styles.socialButton}
+        onClick={handleLogin_Google}
+      >
+        Iniciar Sesión con Google
+      </button>{" "}
+      {/* Aplica la clase de estilo al botón */}
+      <button type="button" className={styles.socialButton}>
+        Iniciar Sesión con Facebook
+      </button>{" "}
+      {/* Aplica la clase de estilo al botón */}
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
