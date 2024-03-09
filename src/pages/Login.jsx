65497fe23f296f4c9012d585a6c9184e1483e086
Gustavo <gmerida@correo.unimet.edu.ve>
2024-03-09 00:49:34 -0400

    Primeros pasos de registro

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import styles from './Login.module.css'
 
 function Login() {
+  const handleEmailLogin = () => {
+      // Lógica para iniciar sesión con correo electrónico y contraseña
+  };
+
+  const handleGoogleLogin = () => {
+      // Lógica para iniciar sesión con Google
+  };
+
+  const handleTwitterLogin = () => {
+      // Lógica para iniciar sesión con Twitter
+  };
+
   return (
-    <div>
-      <h1>Este es el login</h1>
-    </div>
-  )
+    <div className={styles.loginContainer}> {/* Aplica la clase de estilo al contenedor principal */}
+    <h1 className={styles.loginTitle}>Inicio de Sesión</h1> {/* Aplica la clase de estilo al título */}
+    <form>
+        <label>Email:</label>
+        <input className={styles.formInput} type="email" placeholder="Correo electrónico" /> {/* Aplica la clase de estilo al input */}
+        <label>Contraseña:</label>
+        <input className={styles.formInput} type="password" placeholder="Contraseña" /> {/* Aplica la clase de estilo al input */}
+        <button type="button" className={styles.loginButton} onClick={handleEmailLogin}>Iniciar Sesión con Email</button> {/* Aplica la clase de estilo al botón */}
+    </form>
+    <button type="button" className={styles.socialButton} onClick={handleGoogleLogin}>Iniciar Sesión con Google</button> {/* Aplica la clase de estilo al botón */}
+    <button type="button" className={styles.socialButton} onClick={handleTwitterLogin}>Iniciar Sesión con Twitter</button> {/* Aplica la clase de estilo al botón */}
+</div>
+  );
 }
 
-export default Login
+export default Login;
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import Register from './pages/Register'
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
     {
         path:'/login',
         element:<Login/>
+    },
+    {
+        path:'/register',
+        element:<Register/>
     },
     {
         path:'/user/profile',
