f9ef845d390eaf40cc6770090d26487123f27663
Daniel Fiallo <daniel.fiallo@correo.unimet.edu.ve>
2024-03-09 23:40:02 -0400

    Actualización

--- src/router.jsx
+++ src/router.jsx
@@ -1,26 +1,30 @@
 /* eslint-disable no-unused-vars */
-import { createBrowserRouter } from 'react-router-dom'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import AppLayout from './layout/AppLayout'
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import AppLayout from "./layout/AppLayout";
+import Register2 from "./pages/Register2";
 
 export const router = createBrowserRouter([
-    {
-        path:'/user/profile',
-        element:<Home/>
-    },
-    {
-        path:'/login',
-        element:<Login/>
-    },
-    {
-        path:'/register',
-        element:<Register/>
-    },
-    {
-        path:'/',
-        element:<Home/>
-    },
-
-])
+  {
+    path: "/user/profile",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/register2",
+    element: <Register2 />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
