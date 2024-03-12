5193e26e28df631eec98535b7db980c67c4d71e1
Daniel Fiallo <daniel.fiallo@correo.unimet.edu.ve>
2024-03-11 22:12:22 -0400

    Listo

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect  } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, InputBase, Button, Menu, MenuItem } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Button,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import { logOut } from "../controllers/auth";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../context/user";
@@ -15,24 +24,24 @@ import { updateUsers } from "../services/users.js";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginBottom: '50px',
+    marginBottom: "50px",
   },
   title: {
-    fontSize: '25px',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    font: 'inherit',
-    color: 'white',
+    fontSize: "25px",
+    textAlign: "center",
+    fontWeight: "bold",
+    font: "inherit",
+    color: "white",
     flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    width: '50%',
+    width: "50%",
   },
   appBar: {
-    backgroundColor: 'purple', // Cambiar color
-    borderRadius: '10px',
+    backgroundColor: "purple", // Cambiar color
+    borderRadius: "10px",
   },
   // search: {
   //   position: 'relative',
@@ -50,19 +59,19 @@ const useStyles = makeStyles((theme) => ({
   // },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
+    height: "100%",
     //position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
-    cursor: 'pointer', 
-    position: 'relative',
-    backgroundColor: 'none',
-    '&:hover': {
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    cursor: "pointer",
+    position: "relative",
+    backgroundColor: "none",
+    "&:hover": {
       backgroundColor: theme.palette.common.black,
-    }
+    },
   },
 }));
 
@@ -74,57 +83,63 @@ const NavigationBar = () => {
     setAnchorEl(event.currentTarget);
   };
 
-  
   const handleClose = () => {
-      setAnchorEl(null);
-    };
-    
-    const handleEditProfile = () => {
-        // Implementar la lógica para editar el perfil del usuario;
-        navigate("/user/profile");
-        console.log('Editar Perfil');
-        handleClose();
-    };
-    
-    const handleLogout = () => {
-        // Implementar la lógica para cerrar sesión del usuario
-        logOut();
-        console.log('Cerrar Sesión');
-        handleClose();
-    };
-    
-    const user = useUser();
-    const navigate = useNavigate();
-    const [nameInput, setNameInput] = useState("");
-    const [lastname, setLastname] = useState("");
-    const [selectedGame, setSelectedGame] = useState("");
-    const [gameOptions, setGameOptions] = useState([]);
+    setAnchorEl(null);
+  };
 
-    function getUserData(){
-      
-  }
+  const handleEditProfile = () => {
+    // Implementar la lógica para editar el perfil del usuario;
+    navigate("/user/profile");
+    console.log("Editar Perfil");
+    handleClose();
+  };
 
+  const handleLogout = () => {
+    // Implementar la lógica para cerrar sesión del usuario
+    logOut();
+    console.log("Cerrar Sesión");
+    handleClose();
+  };
+
+  const user = useUser();
+  const navigate = useNavigate();
+  const [nameInput, setNameInput] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [selectedGame, setSelectedGame] = useState("");
+  const [gameOptions, setGameOptions] = useState([]);
+
+  function getUserData() {}
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <a  href='/'>
-          <IconButton className={classes.searchIcon} edge="start" color="inherit" aria-label="home">
-            <HomeIcon />
-          </IconButton>
+          <a href="/">
+            <IconButton
+              className={classes.searchIcon}
+              edge="start"
+              color="inherit"
+              aria-label="home"
+            >
+              <HomeIcon />
+            </IconButton>
           </a>
           {/* <div className={classes.search}> */}
-            <a  href='/search'>
+          <a href="/search">
             <IconButton className={classes.searchIcon}>
-                <SearchIcon />
+              <SearchIcon />
             </IconButton>
-            </a>
+          </a>
           {/* </div> */}
           <Typography className={classes.title} variant="h6" noWrap>
             Gaming Clubs
           </Typography>
-          <IconButton edge="end" color="inherit" aria-label="profile" onClick={handleClick}>
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="profile"
+            onClick={handleClick}
+          >
             <AccountCircleIcon />
           </IconButton>
           <Menu
@@ -133,9 +148,8 @@ const NavigationBar = () => {
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
-             
-            <MenuItem onClick={handleEditProfile}>Editar Perfil</MenuItem>
-            <MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
+            <MenuItem onClick={handleEditProfile}>Editar perfil</MenuItem>
+            <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
           </Menu>
         </Toolbar>
       </AppBar>
